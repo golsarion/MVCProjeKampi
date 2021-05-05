b2ce26bd76ddb78dcdfef8f37475a129af7c8b0d
golsarion <golsarion@gmail.com>
2021-05-05 09:29:59 +0300

    Update

---
+++ MvcWebUI/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcWebUI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager();
+        // GET: Category
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+        public ActionResult GetCategoryList()
+        {
+            var categoryvalues = cm.GetAllBL();
+            return View(categoryvalues);
+        }
+    }
+}
