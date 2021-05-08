417431ad80edd78638b6d25fac8685fb54ef2c0c
golsarion <golsarion@gmail.com>
2021-05-08 09:29:28 +0300

    update 08.05.2021

---
+++ DataAccessLayer/Migrations/202105080531345_initializeDB.cs
@@ -0,0 +1,18 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initializeDB : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Categories", "CategoryName", c => c.String(maxLength: 20));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Categories", "CategoryName", c => c.String(maxLength: 50));
+        }
+    }
+}
---
+++ MvcWebUI/Controllers/AdminCategoryController.cs
@@ -0,0 +1,49 @@
+﻿using BussinessLayer.Concrete;
+using BussinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcWebUI.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        CategoryManager _categoryManager = new CategoryManager(new EfCategoryDAL());
+        // GET: AdminCategory
+        public ActionResult Index()
+        {
+            
+            var liste = _categoryManager.GetList();
+            return View(liste);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category parameter)
+        {
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult results = categoryValidator.Validate(parameter);
+            if (results.IsValid)
+            {
+                _categoryManager.Add(parameter);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View(parameter);
+        }
+    }
+}
